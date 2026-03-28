ebabbb9df9061d1f0c793baeaa2324d3e9ed79d7
meyghane <meyghane@gmail.com>
2026-03-28 08:41:41 +0100

    feat: favicon W teal (Next.js App Router icon)

---
+++ src/app/icon.tsx
@@ -0,0 +1,29 @@
+import { ImageResponse } from 'next/og';
+
+export const size = { width: 32, height: 32 };
+export const contentType = 'image/png';
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: 32,
+          height: 32,
+          background: '#0d9488',
+          borderRadius: 8,
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color: 'white',
+          fontSize: 20,
+          fontWeight: 700,
+          fontFamily: 'sans-serif',
+        }}
+      >
+        W
+      </div>
+    ),
+    { ...size }
+  );
+}
